ea643d4552cc0e1df2680e39782ca62e8b263903
doyeongkim <rlaehdud19@gmail.com>
2017-06-13 14:12:51 +0800

    Update function.php
    adding a function for a house icon

--- functions.php
+++ functions.php
@@ -8,3 +8,12 @@ function theme_enqueue_styles(){
    );
 }
  add_action('wp_enqueue_scripts','theme_enqueue_styles');
+
+function wmpudev_enqueue_icon_stylesheet() {
+	wp_register_style( 'fontawesome', 'http:////maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css' );
+	wp_enqueue_style( 'fontawesome');
+}
+ 
+ add_action( 'wp_enqueue_scripts',
+     'wmpudev_enqueue_icon_stylesheet' 
+ );
